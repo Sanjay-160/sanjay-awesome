4196cf1fc288f2120301d27a37ff1029d2abe955
Sanjay-160 <125868814+Sanjay-160@users.noreply.github.com>
2023-12-22 00:15:55 +0530

    server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Buddy')
+    res.end('Hello Cloud Run v2 ' + new Date() )
   })
   .listen(PORT, () => console.log('Listening on', PORT))
